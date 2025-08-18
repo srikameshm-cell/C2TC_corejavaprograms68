0fa8132ba4490ab7cd21d709294078897976d309
srika <srika@SRIKAMESH>
2025-08-18 14:04:41 +0530

    day 2 learn break and contineu

--- corejavaprograms/src/day2_/BreakDemo.java
+++ corejavaprograms/src/day2_/BreakDemo.java
@@ -1,5 +1,13 @@
 package day2_;
 
 public class BreakDemo {
+	public static void main(String[] args) {
 
+		for (int i = 5; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.println(i);
+		}
+
+	}
 }
