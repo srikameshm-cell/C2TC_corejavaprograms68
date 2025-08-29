176a58856718505592df1d06c8042318a38e6409
srikameshm-cell <srikameshm@gmail.com>
2025-08-29 15:41:58 +0530

    day 14 is completed

---
+++ corejavaprograms/src/DAY_14/SAMPLE.java
@@ -0,0 +1,5 @@
+package DAY_14;
+
+public class SAMPLE {
+
+}
--- corejavaprograms/src/day_13/ThreadLifeCycleDemo.java
+++ corejavaprograms/src/day_13/ThreadLifeCycleDemo.java
@@ -1,5 +1,19 @@
 package day_13;
 
+
 public class ThreadLifeCycleDemo {
 
+	public static void main(String[] args) {
+		MyChildThread myThread = new MyChildThread();
+		System.out.println("Before Runnable stage Thread is alive or not? " + myThread.isAlive());
+		myThread.start();
+
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException exp) {
+			System.err.println("Thread is interrupted !");
+		}
+
+		System.out.println("After complete execution of Thread ,it is alive or not? " + myThread.isAlive());
+	}
 }
